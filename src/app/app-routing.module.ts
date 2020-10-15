bf6c55832a198bc96655898de7d7e3bf87cbc63c
willyan <willyanlin@gmail.com>
2020-10-15 15:07:14 +0200

    (feat): add json + contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,21 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
     {
-        path: 'home',
+        path: '',
         loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
     },
     {
         path: '',
-        redirectTo: 'home',
+        redirectTo: '',
         pathMatch: 'full'
     },
     {
         path: 'restaurants-list',
         loadChildren: () => import('./pages/restaurants-list/restaurants-list.module').then(m => m.RestaurantsListPageModule)
+    },
+    {
+        path: 'contact',
+        loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactPageModule)
     },
 
 ];
