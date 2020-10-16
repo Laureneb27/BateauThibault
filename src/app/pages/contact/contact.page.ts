77f2812a2cf2e431dea5cfc7cf0d9283af1ca73e
willyan <willyanlin@gmail.com>
2020-10-16 12:38:45 +0200

    (feat): add final contact page

--- src/app/pages/contact/contact.page.ts
+++ src/app/pages/contact/contact.page.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactPage implements OnInit {
 
+  user = {
+    name: 'John Thibault',
+    avatar: 'assets/images/contact/ian-avatar.png',
+    description: 'Si vous avez besoin d\'informations complémentaires n\'hésitez pas à me contacter',
+    address: '12 Rue de Cléry, 75002 Paris',
+    phone: '+33 12 34 56 78',
+    email: 'j.thibault@cfa-insta.fr',
+    whatsapp: '555 555 555',
+  };
+
   constructor() { }
 
   ngOnInit() {
