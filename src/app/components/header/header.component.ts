313ccda38436dfbc175c2ce816763fb7668c0868
willyan <willyanlin@gmail.com>
2020-10-18 23:45:04 +0200

    (feat): change menu order and icon, set panier clickable

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
-  ngOnInit() {}
+  navigateTo(routeName) {
+    this.router.navigate([routeName])
+  }
+  ngOnInit() { }
 
 }
